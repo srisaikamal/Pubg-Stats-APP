fdc331152f1110daa8632acfc9f590205c82f151
Kamal Vasamsetti <srisaikamal2000@gmail.com>
2020-06-29 12:21:48 +0530

    Google Analytics integrated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./Pages/Home";
 import PubgState from "./Context/PubgState";
+import ReactGA from "react-ga";
 
-function App() {
+const GoogleAnalytics = () => {
+  ReactGA.initialize("UA-137410114-6");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+};
+
+const App = () => {
+  useEffect(() => {
+    GoogleAnalytics();
+  }, []);
+
   return (
     <PubgState>
       <Home />
     </PubgState>
   );
-}
+};
 
 export default App;
